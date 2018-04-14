c85e5fac26b976e0da8a061afc3685c97f303d76
flukyysupp@gmail.com <flukyysupp1@gmail.com>
2018-04-15 04:07:31 +0700

    commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals, print_function
 from flask import Flask, request, abort
 import os
-import sys
 import errno
 import tempfile
 from argparse import ArgumentParser
@@ -15,11 +14,13 @@ from linebot.models import (
     TextSendMessage, MessageEvent, ImageMessage, TextMessage
 )
 
-import json
-import requests
+
 LINE_API = 'https://api.line.me/v2/bot/message/reply'
 handler = WebhookHandler('058e9407061ba0bf6cef25392fcd34df')
-line_bot_api = LineBotApi('DXYPEtAqiUkn9e2HyPughfjyafbrCxT4nBZ52rDf1UKDSvZcWI3G9OKgexXggWZRER9ml7RAmTUjElHzAPzBVtVwzfXjin25UzjsJKz75TenY1BshnLWgIDbxyKZp3G1yhigMP08ihMxG6pkr6rfEQdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('DXYPEtAqiUkn9e2HyPughfjyafbrCxT4nBZ52rDf1U'
+                          'KDSvZcWI3G9OKgexXggWZRER9ml7RAmTUjElHzAPzBV'
+                          'tVwzfXjin25UzjsJKz75TenY1BshnLWgIDbxyKZp3G1y'
+                          'higMP08ihMxG6pkr6rfEQdB04t89/1O/w1cDnyilFU=')
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 def make_static_tmp_dir():
     try:
@@ -32,7 +33,7 @@ def make_static_tmp_dir():
 
 
 app = Flask(__name__)
-@app.route('/')
+@app.route ('/')
 def index():
   return "Hello World!"
 
@@ -63,13 +64,14 @@ def handle_text_message(event):
     )
 
 
-@handler.add(MessageEvent, message = ImageMessage)
+@handler.add(MessageEvent, message=ImageMessage)
 def handle_image_message(event):
 
     message_content = line_bot_api.get_message_content(event.message.id)
     line_bot_api.reply_message(
         event.reply_token, [
-            TextSendMessage(text=str(message_content))
+            TextSendMessage(text=message_content.iter_content()[0]),
+                            TextSendMessage(text=message_content.iter_content()[1])
         ]
     )
 
