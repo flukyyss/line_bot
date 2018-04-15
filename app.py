278b45ead8108f8ead0f7d12c4563e7fde058e4d
flukyysupp@gmail.com <flukyysupp1@gmail.com>
2018-04-15 08:10:35 +0700

    commit

--- app.py
+++ app.py
@@ -78,6 +78,7 @@ def handle_image_message(event):
                 count+=1
                 f.write(chunk)
         print('success')
+        print(f.name)
     except FileNotFoundError as e:
         line_bot_api.reply_message(
         event.reply_token, [
