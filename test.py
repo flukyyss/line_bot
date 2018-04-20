79a1a37ea21ecb3043b3cfb56aa5659e51aef61f
flukyysupp@gmail.com <flukyysupp1@gmail.com>
2018-04-20 23:34:33 +0700

    'commit'

--- test.py
+++ test.py
@@ -1,4 +1,28 @@
-from tempfile import NamedTemporaryFile
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
 
-with open(file=r'C:\Users\fluky\Desktop\New folder (2)\New folder\static\tmp/info.txt',mode="w") as ft:
-    ft.write("hello")
+img1 = cv2.imread(r'C:\Users\fluky\Desktop\New folder (2)\New folder\pat1.jpg',0)          # queryImage
+img2 = cv2.imread(r'C:\Users\fluky\Desktop\New folder (2)\New folder\pat2.jpg',0) # trainImage
+
+# Initiate SIFT detector
+sift = cv2.SIFT()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+
+# BFMatcher with default params
+bf = cv2.BFMatcher()
+matches = bf.knnMatch(des1,des2, k=2)
+
+# Apply ratio test
+good = []
+for m,n in matches:
+    if m.distance < 0.75*n.distance:
+        good.append([m])
+
+# cv2.drawMatchesKnn expects list of lists as matches.
+img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,flags=2)
+
+plt.imshow(img3),plt.show()
