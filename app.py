8774ad6445ea3fc04acb9ab232c51ece75b79b10
flukyysupp@gmail.com <flukyysupp1@gmail.com>
2018-04-21 17:39:32 +0700

    'commit'

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def handle_text_message(event):
     else:
         line_bot_api.reply_message(
             event.reply_token, [
-                TextSendMessage(text='ไม่เข้าใจคำสั่งครับ')
+                TextSendMessage(text=event.message.text)
 
             ]
         )
