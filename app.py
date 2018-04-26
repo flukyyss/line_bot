c33bf911186e63ad8e15e2352114dc43c83d89db
flukyysupp@gmail.com <flukyysupp1@gmail.com>
2018-04-27 00:41:52 +0700

    'commit'

--- app.py
+++ app.py
@@ -154,17 +154,17 @@ def handle_image_message(event):
         print('resize')
         print(im2.size[0])
         print(im2.size[1])
-    for n in range(im2.size[0]):
+    for n in range(im2.size[0]): #
         for r in range(im2.size[1]):
             pixel1 = rgb_im.getpixel((n, r))
             pixel2 = rgb_im2.getpixel((n, r))
-            color1_rgb = sRGBColor(pixel1[0],pixel1[1],pixel1[2])
-            color2_rgb = sRGBColor(pixel2[0],pixel2[1],pixel2[2])
-            color1_lab = convert_color(color1_rgb, LabColor)
-            color2_lab = convert_color(color2_rgb, LabColor)
-            delta_e = delta_e_cie2000(color1_lab,color2_lab)
-            if(delta_e<15):
-                count+=1
+            # color1_rgb = sRGBColor(pixel1[0],pixel1[1],pixel1[2])
+            # color2_rgb = sRGBColor(pixel2[0],pixel2[1],pixel2[2])
+            # color1_lab = convert_color(color1_rgb, LabColor)
+            # color2_lab = convert_color(color2_rgb, LabColor)
+            # delta_e = delta_e_cie2000(color1_lab,color2_lab)
+            # if(delta_e<15):
+            #     count+=1
         if(n%10==0):
             print(n)
     print(count/(im2.size[0]*im2.size[1]))
