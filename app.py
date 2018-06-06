a6f65383fe3b9f62d2bae5431b964bd3208536cb
flukyysupp@gmail.com <flukyysupp1@gmail.com>
2018-06-06 17:36:29 +0700

    'updated'

--- app.py
+++ app.py
@@ -307,7 +307,6 @@ def handle_image_message(event):
         ])
     print(similarity)
     print(minsim)
-    print(indexmin)
     '''''''''''
     line_bot_api.reply_message(
         event.reply_token, [
